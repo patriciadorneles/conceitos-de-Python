2551ceeb3b6ad6f4bcaf05fc92cc5a8499619a2c
Patrícia Dorneles <104579015+patriciadorneles@users.noreply.github.com>
2022-10-06 15:41:50 -0300

    Add files via upload

---
+++ dir_e_help.py
@@ -0,0 +1,11 @@
+""""
+Utilitários Python que auxiliam na programação;
+
+dir --> Apresenta todos os atributos/propriedades, funções e métodos disponíveis para determinado tipo de dado
+ou variável.
+
+help --> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para
+determinado tipo de dado ou variável.
+
+"""
+
